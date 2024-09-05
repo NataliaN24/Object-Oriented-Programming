b002a20bd2104f99e607096d73542e040f767ad7
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-09-05 17:13:24 +0300

    Delete Exams Solutions/2023-2024/TEST 02 directory

--- Exams Solutions/2023-2024/TEST 02/Rock Paper Scissors/GameObjectsFactory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <fstream>
-#include "GameObject.h"
-
-Scissors* createScissors(std::ifstream& ifs);
-Rock* createRock(std::ifstream& ifs);
-Paper* createPaper(std::ifstream& ifs);
-GameObject* gameObjFactory(std::ifstream& ifs);
