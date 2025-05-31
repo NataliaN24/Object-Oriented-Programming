13b3966cce6ebe7a5de92b8ea251e78289beb974
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-05-31 20:44:17 +0300

    Update Answers.h

--- Lecture 01/Test Sample/Answers.h
+++ Lecture 01/Test Sample/Answers.h
@@ -5,15 +5,24 @@
 constexpr int MAX_NUM_OPTIONS = 4;
 constexpr int MAX_NUM_ANSWER_DESCRIPTION = 50;
 
+enum class Options {
+	A,
+	B,
+	C,
+	D
+};
+
 struct Answer {
 
 	char answerDescription[MAX_NUM_OPTIONS][MAX_NUM_ANSWER_DESCRIPTION + 1]{};
-
+	Options correctOption;
 public:
 
 	Answer();
-	Answer(char answer[][MAX_NUM_ANSWER_DESCRIPTION + 1]);
-	void setAnswerForEachOption( char answer[][MAX_NUM_ANSWER_DESCRIPTION + 1]);
+	Answer(char answer[][MAX_NUM_ANSWER_DESCRIPTION + 1], Options correct);
+	void setAnswerForEachOption( char answer[][MAX_NUM_ANSWER_DESCRIPTION + 1],Options correct);
 	void print()const;
+	bool isCorrect(int choice)const;
+
 
 };
