304961eccfba09a5f14ecbbcdfec074e4fcea8f5
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-05-31 20:44:48 +0300

    Update Test.h

--- Lecture 01/Test Sample/Test.h
+++ Lecture 01/Test Sample/Test.h
@@ -8,10 +8,14 @@ class Test
 	Questions questions;
 	Answer answers[MAX_NUM_QUESTIONS];
 	size_t points[MAX_NUM_QUESTIONS];
+	size_t totalScore = 0;
 
 public:
 	Test() = default;
 	void setTest(const Questions&questions,const Answer *answersDescription,const size_t points[]);
 	void print()const;
+	void runTest();
+	size_t getTotalPointsOfUser()const;
+
 };
 
